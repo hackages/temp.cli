cb64de2739e353ed0b626d8f3da2697f355b66ff
davyengone <engonenzedavy@yahoo.com>
2016-05-07 19:48:14 +0200

    added angular and angular-mocks for testing

--- tools/config/karma.conf.js
+++ tools/config/karma.conf.js
@@ -1,35 +1,46 @@
 // Karma configuration file;
 const path = require('path');
+const glob = require('glob');
 const context = process.cwd();
 const nodeModules = path.resolve(__dirname, '../node_modules');
 
+// maybe it could be a good idea to remove angular from here
+// Let's the develper import what he needs
 const localDeps = [
-  'jasmine-expect/dist/jasmine-matchers.js',
-  'jasmine-promise-matchers/dist/jasmine-promise-matchers.js',
+  'angular/angular.js',
+  'angular-mocks/angular-mocks.js',
+  'phantomjs-polyfill/bind-polyfill.js',
 ].map(file => path.join(nodeModules, file));
 
-const externaleDeps = [
-  'components/*/test/**/*.spec.js',
-].map(file => path.join(context, file));
+const basePath = glob.sync('components/*').length > 0 ? 'components/*/' : '';
+
+const paths = {
+  scripts: `${basePath}test/**/*.spec.js`,
+  tests: `${basePath}scripts/**/test/**/*.spec.js`,
+};
+
+const externaleDeps = [paths.scripts].map(file => path.join(context, file));
 
 const files = localDeps.concat(externaleDeps);
+
 const reporters = ['mocha', 'coverage'];
 
 // preprocessors configuration
-const testsFiles = path.resolve(context, 'components/*/test/**/*.spec.js');
-const scriptsFiles = path.resolve(context, 'components/*/scripts/**/test/**/*.spec.js');
+const testsFiles = path.resolve(context, paths.scripts);
+const scriptsFiles = path.resolve(context, paths.tests);
 
-// Preprocessors and plugins: webpack and coverage
-const preprocessors = {};
+// Preprocessors and plugins: webpack and sourcemap
 const plugins = ['webpack', 'sourcemap'];
-preprocessors[testsFiles] = plugins;
-preprocessors[scriptsFiles] = plugins;
+const preprocessors = {
+  [scriptsFiles]: plugins,
+  [testsFiles]: plugins,
+};
 
 module.exports = (config) => {
   config.set({
+    quiet: true,
     frameworks: ['jasmine'],
     files,
-    quiet: true,
     preprocessors,
     webpackMiddleware: {
       noInfo: true,
