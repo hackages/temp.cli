38e5c15752b28df41ef176944053ae54d6a6627f
davyengone <engonenzedavy@yahoo.com>
2016-05-09 05:44:44 +0200

    architecture step 1: fixing test

--- components/widget-hello-world/test/unit/hello.spec.js
+++ components/widget-hello-world/test/unit/hello.spec.js
@@ -1,16 +1,23 @@
-describe('Testing hello world', function(){
-beforeEach(module('app'));
 
-describe('hello world widget',() => {
-    var vm;
+describe('Testing hello world', function(){
+
+  function Controller () {
+    this.message = 'Hello World';
+  }
+
+  angular.module('app', []).controller('MainCtrl', Controller);
+
+  beforeEach(angular.mock.module('app'));
+
+  describe('hello world widget',() => {
+    let vm;
     beforeEach(inject(function ($controller) {
-        vm = $controller('MainCtrl',{});
+      vm = $controller('MainCtrl');
     } ));
 
     it('should be a text', function() {
-        expect(vm.message).toBe('Hello World');
+      expect(vm.message).toBe('Hello World');
     });
-
-})
-})
+  });
+});
 
