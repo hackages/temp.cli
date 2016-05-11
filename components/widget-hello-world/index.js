62145a40bb0dab41ebd3a30fafd0225e4e970d6d
Kathleen Lesouple <kathleen.lesouple@crelan.be>
2016-05-11 15:13:15 +0200

    BACKBASE-38: DIGIBANK-38: Add two columns container

--- components/widget-hello-world/index.js
+++ components/widget-hello-world/index.js
@@ -2,6 +2,3 @@ import account from './modules/account';
 
 angular.module('app', [account]);
 
-document.addEventListener('DOMContentLoaded', function () {
-  angular.bootstrap(document.body, ['app']);
-});
--- components/widget-navigation/index.js
+++ components/widget-navigation/index.js
@@ -1,7 +1,3 @@
 import navigation from './modules/navigation';
 
 angular.module('app', [navigation]);
-
-document.addEventListener('DOMContentLoaded', function () {
-  angular.bootstrap(document.body, ['app']);
-});
