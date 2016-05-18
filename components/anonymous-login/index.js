dceff23cecd4d6510ce0b97f6413fa4f0335ab1f
Kathleen Lesouple <kathleen.lesouple@crelan.be>
2016-05-18 14:19:14 +0200

    BACKBASE-53: add anonymous login widget

---
+++ components/anonymous-login/index.js
@@ -0,0 +1,7 @@
+/*import mocks from './mocks';*/
+
+angular.module('app', [/*mocks*/]);
+
+document.addEventListener('DOMContentLoaded', function () {
+  angular.bootstrap(document.body, ['app']);
+});
