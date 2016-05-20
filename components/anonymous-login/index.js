5fc26b490c8dfd795aab60041c72addd2ceab4ad
Kathleen Lesouple <kathleen.lesouple@crelan.be>
2016-05-20 13:29:08 +0200

    BACKBASE-53: renamed anonymous-login to accounts-login

--- components/anonymous-login/index.js
+++
@@ -1,9 +0,0 @@
-import anonymousLogin from './modules/anonymouslogin'
-
-const crelanAnonymousLogin = 'crelan.anonymouslogin';
-
-angular.module(crelanAnonymousLogin, [anonymousLogin]);
-
-document.addEventListener('DOMContentLoaded', function () {
-  angular.bootstrap(document.body, [crelanAnonymousLogin]);
-});
