b602d6010bfbea3d5431fe6a60e496e5e022fe62
Kathleen Lesouple <kathleen.lesouple@crelan.be>
2016-05-23 11:19:29 +0200

    BACKBASE-53: Add empty digipass identification widget

---
+++ components/digipass-identification/index.js
@@ -0,0 +1,6 @@
+const crelanIdentifier = 'crelan.identifier';
+angular.module(crelanIdentifier, []);
+
+document.addEventListener('DOMContentLoaded', function () {
+  angular.bootstrap(document.body, [crelanIdentifier]);
+});
