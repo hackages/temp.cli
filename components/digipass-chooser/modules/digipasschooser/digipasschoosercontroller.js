713f7fa999fc997c3d41ef99b6efedb5ff8870e5
Kathleen Lesouple <kathleen.lesouple@crelan.be>
2016-05-23 10:55:08 +0200

    BACKBASE-53: Removed unused files

--- components/digipass-chooser/modules/digipasschooser/digipasschoosercontroller.js
+++
@@ -1,5 +0,0 @@
-function Digipasschoosercontroller() {
-  
-}
-
-export default Digipasschoosercontroller;
--- components/digipass-chooser/modules/digipasschooser/digipasschooserdirective.js
+++ components/digipass-chooser/modules/digipasschooser/digipasschooserdirective.js
@@ -1,17 +1,13 @@
 import angular from 'angular/index.js';
 import digipassTemplate from './digipass.html';
-import Digipasschoosercontroller from './digipasschoosercontroller';
-/*import digipassChooserService from './services/digipasschooserservice';*/
 
-let module = angular.module('crelan.digipassChooserDirective', [/*digipassChooserService*/])
+let module = angular.module('crelan.digipassChooserDirective', [])
   .directive('digipassChooser', digipassChooserDirective);
 export default module.name;
 
 function digipassChooserDirective(){
   return {
     restrict: 'EA',
-    template: digipassTemplate,
-    scope: {},
-    controller: Digipasschoosercontroller
+    template: digipassTemplate
   }
 }
