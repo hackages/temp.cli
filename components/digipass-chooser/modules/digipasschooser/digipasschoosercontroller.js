25669581e97b9fa0df0f7a4d73a727f610786906
Kathleen Lesouple <kathleen.lesouple@crelan.be>
2016-05-23 10:52:56 +0200

    BACKBASE-53: removed unused code in controller

--- components/digipass-chooser/modules/digipasschooser/digipasschoosercontroller.js
+++ components/digipass-chooser/modules/digipasschooser/digipasschoosercontroller.js
@@ -1,11 +1,5 @@
 function Digipasschoosercontroller() {
-
-  gadgets.pubsub.subscribe('channelA', function(msg) {
-    console.log("subscribe To A");
-    // a callback function to display incoming messages on channel
-    $('#messageHolderA').append(msg + '<br/>');
-  });
-
+  
 }
 
 export default Digipasschoosercontroller;
