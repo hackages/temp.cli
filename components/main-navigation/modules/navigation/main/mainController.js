3177689abbb075387972cc4b38135ece1b0ddb4b
Oscar Lodriguez <oscar.lodriguez@crelan.be>
2016-05-23 12:04:53 +0200

    C:/Program Files/Git/BACKBASE-164

--- components/main-navigation/modules/navigation/main/mainController.js
+++ components/main-navigation/modules/navigation/main/mainController.js
@@ -1,22 +1,5 @@
-<<<<<<< HEAD:components/main-navigation/modules/navigation/main/mainController.js
 function MainController() {
-    this.getNavigation = function() {
-        return [
-            {"label": "Dashboard", "url": "#"},
-            {"label": "Rekeningen &amp; kaarten", "url": "#"},
-            {"label": "Betalen", "url": "#"},
-            {"label": "Berichten &amp; documenten", "url": "#"},
-            {"label": "Beleggen", "url": "#"},
-            {"label": "Kredieten", "url": "#"},
-            {"label": "Verzekeringen", "url": "#"}
-        ];
-    }
-
-}
-
-=======
-function MainController() {
-  this.getNavigation = function() {
+  this.getNavigation = () => {
     return [
       {"label": "Dashboard", "url": "#"},
       {"label": "Rekeningen &amp; kaarten", "url": "#"},
@@ -27,8 +10,6 @@ function MainController() {
       {"label": "Verzekeringen", "url": "#"}
     ];
   }
-
 }
 
->>>>>>> f51d1668b1ed566ae31365459a6b8d318e64851a:components/widget-main-navigation/modules/navigation/main/mainController.js
 export default MainController;
