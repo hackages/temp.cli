8c4ceef0295eceb87ee50227e6cb8e0213ed34f0
Kathleen Lesouple <kathleen.lesouple@crelan.be>
2016-05-23 11:06:17 +0200

    BACKBASE-53: Removed unused controller

--- components/accounts-login/modules/anonymouslogin/anonymouslogindirective.js
+++ components/accounts-login/modules/anonymouslogin/anonymouslogindirective.js
@@ -1,6 +1,5 @@
 import angular from 'angular/index.js';
 import anonymousLoginTemplate from './anonymouslogin.html';
-import anonymousLogonCtrl from './anonymouslogincontroller';
 
 let module = angular.module('crelan.anonymousLogin.anonymousLoginDirective', [])
   .directive('anonymousLogin', anonymousLoginDirective);
@@ -9,8 +8,6 @@ export default module.name;
 function anonymousLoginDirective(){
   console.log("directive");
   return {
-    template: anonymousLoginTemplate,
-    controller: anonymousLogonCtrl,
-    controllerAs: 'anonymousLogonCtrl'
+    template: anonymousLoginTemplate
   }
 }
