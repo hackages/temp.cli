01664a66f45dc16784b8557bc5b151badb0c883a
Ian Corne (Consultant) <ian.corne@crelan.be>
2016-05-24 11:31:00 +0200

    BACKBASE-73: Add list - grid switch

--- components/accounts-overview/modules/accountsoverview/accountsoverviewctrl.js
+++ components/accounts-overview/modules/accountsoverview/accountsoverviewctrl.js
@@ -5,6 +5,7 @@ import {AccountsOverviewServiceName} from './services/accountsoverviewservice'
  * @ngInject AccountsOverviewService
  */
 const AccountsOverviewController = function(AccountsOverviewService) {
+    this.grid = true;
     const accountsPromise = AccountsOverviewService.getAllAccounts();
     accountsPromise.then((result) => {
         this.allAccounts =  result.payload;
@@ -12,6 +13,9 @@ const AccountsOverviewController = function(AccountsOverviewService) {
         console.error(error);
         throw error;
     })
+    this.toggleGridView = () => {
+        this.grid = !this.grid;
+    }
 };
 AccountsOverviewController.$inject = [AccountsOverviewServiceName];
 export default AccountsOverviewController;
