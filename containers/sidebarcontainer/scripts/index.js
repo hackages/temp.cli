5a5b68418fe61c39ac7388f661c13fccd2881c13
Oscar Lodriguez <oscar.lodriguez@crelan.be>
2016-05-24 06:14:55 +0200

    BACKBASE-164 theme

--- containers/sidebarcontainer/scripts/index.js
+++
@@ -1,7 +0,0 @@
-
-//document.addEventListener('DOMContentLoaded', function () {
-  window.counter = window.counter || 0;
-
-  console.log('inside loader', window.counter);
-  angular.module('crelanApp', []);
-//});
