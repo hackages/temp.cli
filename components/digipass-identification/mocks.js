97e64080a4a613ca5ea3766357ee142e3dcee168
Ian Corne (Consultant) <ian.corne@crelan.be>
2016-05-27 11:53:41 +0200

    BACKBASE-229: make mocks dynamic

--- components/digipass-identification/mocks.js
+++ components/digipass-identification/mocks.js
@@ -1,11 +1,24 @@
 import angular from 'angular/index.js';
 import 'angular-mocks';
-import logonInfo from './mocks/logon_success.json';
+import logonFail from './mocks/logon_fail.json';
+import logonSuccess from './mocks/logon_success.json';
+
+
+const logonResponse = (method, url, data, headers, params) => {
+  window.arguments = [arguments, method, url, data, headers, params];
+  const userCode = (JSON.parse(data)).userCode;
+  if ('ERRORS' === userCode) {
+    return  [200, logonFail];
+  }
+  return [200, logonSuccess];
+};
 
 const mockConfiguration = ($httpBackend) => {
-  $httpBackend.whenPOST('/portal/auth/logon').respond(logonInfo);
+  $httpBackend.whenPOST('/portal/auth/logon').respond(logonResponse);
   $httpBackend.whenPOST().passThrough();
 };
+
+
 mockConfiguration.$inject = ['$httpBackend'];
 
 const module = angular.module('mocks', ['ngMockE2E'])
