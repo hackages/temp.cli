62df53cf4a9fd48b07ee5e7dece88ac05dcf7f64
davyengone <engonenzedavy@yahoo.com>
2016-06-07 03:53:22 +0200

    renamed build and watch files

--- tools/parser/webpack.debug.js
+++
@@ -1,9 +0,0 @@
-import cliparse from 'cliparse';
-import build from '../webpackRunner';
-
-const debugCMD = cliparse.command('debug', {
-  description: 'Get a better a description of your errors from webapck',
-},
-build.bind(null, { options: { debug: true } }));
-
-export default debugCMD;
