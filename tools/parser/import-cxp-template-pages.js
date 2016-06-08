1921c4234afe411b03d8c0b1547794438ab7a6a8
tomcat <tomcat@hn176.crelan.be>
2016-06-08 18:07:20 +0200

    fix deploy cxp template pages

--- tools/parser/import-cxp-template-pages.js
+++ tools/parser/import-cxp-template-pages.js
@@ -1,18 +1,18 @@
 /**
- * CLI tools: Import all cxp features
+ * CLI tools: Import all cxp template pages
  **/
 import cliparse from 'cliparse';
 import config from '../config/configuration';
 import { zipImport } from '../config/utils';
 
 export const configuration = {
-  items: `${config.context}/cxp-template-page`,
+  items: `${config.context}/cxp-template-pages`,
   toZip: ['.'].join(' '),
   target: '.',
 };
 
-const cmd = cliparse.command('import-features', {
-  description: 'Import all cxp features',
+const cmd = cliparse.command('import-cxp-template-pages', {
+  description: 'Import all cxp template pages',
 },
 zipImport.bind(null, configuration));
 
